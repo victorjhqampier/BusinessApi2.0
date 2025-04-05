731c38722bd6ff284d7ecf0a3856f0607c11b1fe
Deepsky Skynet <victorjhampier@gmail.com>
2025-04-05 01:37:51 -0500

    added cognitoAuth and refact http-connector pool ssl

--- src/Presentation/Controllers/ExampleController.py
+++ src/Presentation/Controllers/ExampleController.py
@@ -3,6 +3,7 @@ from Application.Adpaters.ExampleAdapters.ExampleRequestAdaper import ExampleReq
 from Application.Helpers.EasyResponseCoreHelper import EasyResponseCoreHelper
 from Application.Usecases.ExampleCase.ExampleUsecase import ExampleUsecase
 from Presentation.Handlers.ArifyAuthorizer import ArifyAuthorizer
+from Presentation.Handlers.CognitoAuthorizer import CognitoAuthorizer
 from Presentation.Handlers.ScopesHandler import ScopesHandler
 from fastapi import APIRouter, Security, Depends, Header, Path, Query
 from fastapi.responses import JSONResponse
@@ -28,8 +29,33 @@ async def get_user_for_example(
     customer_id: int = Path(..., title="Customer ID", description="ID único del cliente"),
     include_details: bool = Query(False, title="Include Details", description="Si es True, devuelve más información"),
     auth: str = Header(..., title="Authorization", description="Token de autenticación"),
-    usecase: ExampleUsecase = Depends(ExampleUsecase),  # Inyección de dependencia
-    secury: dict = Security(ArifyAuthorizer(), scopes=[ScopesHandler.READ])
+    usecase: ExampleUsecase = Depends(ExampleUsecase),  # Inyección directa de dependencia
+    _: dict = Security(ArifyAuthorizer(), scopes=[ScopesHandler.READ])
+):
+    try:
+        response: CreateExampleAdapter = await usecase.get_client_async(request)
+
+        return JSONResponse(
+            status_code=response.statusCode,
+            content=jsonable_encoder(response, exclude_none=True)
+        )
+    
+    except Exception as ex:
+        track:str = traceback.format_exc()
+        Logger.error(track.replace('\n',' ').strip())
+        return JSONResponse(
+            status_code=500,
+            content=jsonable_encoder(EasyResponse.EasyErrorRespond("99","Ocurrió el siguiente error: "+ str(ex)))
+        )
+# Create a route for the get_user_for_example method
+@ExampleController.post("/{customer_id}/new/create", response_model=CreateExampleAdapter)
+async def get_user_for_example2(    
+    request: ExampleRequestAdaper, # Request Body
+    customer_id: int = Path(..., title="Customer ID", description="ID único del cliente"),
+    include_details: bool = Query(False, title="Include Details", description="Si es True, devuelve más información"),
+    auth: str = Header(..., title="Authorization", description="Token de autenticación"),
+    usecase: ExampleUsecase = Depends(ExampleUsecase),  # Inyección directa de dependencia
+    _: dict = Security(CognitoAuthorizer(), scopes=["openid"])
 ):
     try:
         response: CreateExampleAdapter = await usecase.get_client_async(request)
